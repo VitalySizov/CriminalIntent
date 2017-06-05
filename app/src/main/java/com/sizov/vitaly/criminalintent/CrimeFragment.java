49952087e3ba292c3e8ee80b0070752dbe7e1746
Vitaly Sizov <vitalii_sizov@mail.ru>
2017-06-05 22:24:48 +0600

    Chapter 8 Completed

--- app/src/main/java/com/sizov/vitaly/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/sizov/vitaly/criminalintent/CrimeFragment.java
@@ -8,11 +8,16 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment{
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,6 +47,21 @@ public class CrimeFragment extends Fragment{
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        // Установка даты на кнопку и блокирование нажатия
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        // Назначение слушателя для изменений CheckBox
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Назначение раскрытия преступления
+                mCrime.setSolved(isChecked);
             }
         });
 
